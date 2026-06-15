6d8a1ebb709d56fe84d7629ecdfb41d2f42d0609
chu723204-coder <chu723204@gmail.com>
2026-06-15 15:18:39 +0900

    [수정]로그인 기능 오류 및 버그 수정
    [수정]로그인 기능 오류 및 버그 수정

--- DevLink_front/DevLink-Front/src/App.tsx
+++ DevLink_front/DevLink-Front/src/App.tsx
@@ -1,7 +1,23 @@
 import AppRouter from './routers/AppRouter'
+import { Toast } from './components/common/Toast'
+import { useToastStore } from './store/toastStore'
 
 function App() {
-  return <AppRouter />
+  const { hideToast, isVisible, message, variant, position } = useToastStore()
+
+  return (
+    <>
+      <AppRouter />
+      <Toast
+        variant={variant}
+        position={position}
+        isVisible={isVisible}
+        onClose={hideToast}
+      >
+        {message}
+      </Toast>
+    </>
+  )
 }
 
 export default App
